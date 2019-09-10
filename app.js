8a087eda4cf70f638ebd25969bf4ab571067328e
Juank628 <jsanchez_zunino@yahoo.com>
2019-09-10 17:27:19 -0500

    order post response

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const mongoose = require("mongoose")
+
 const Product = require("./models/Product");
 const Order = require("./models/Order");
 
@@ -27,7 +29,7 @@ app.get("/api/search/:family/:subfamily", async (req, res) => {
   } catch (error) {
     res.status(500);
     res.json({
-      error
+      error: error.message
     });
   }
 });
@@ -37,12 +39,19 @@ app.post("/api/order", async (req, res) => {
     let order = new Order({
       ...req.body
     });
-    await order.save();
-    res.status(201).end();
+    const newDocument = await order.save();
+    const objectId = mongoose.Types.ObjectId(newDocument.id)
+    const timeStamp = objectId.getTimestamp()
+    const id = objectId.toString().slice(18)
+    res.status(201);
+    res.json({
+      id,
+      timeStamp
+    });
   } catch (error) {
     res.status(500);
     res.json({
-      error
+      error: error.message
     });
   }
 });
